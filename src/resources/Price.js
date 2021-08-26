1d1fa72e12dc62380cbb6278ca6c84724eff9710
Pedro Henrique Arthur <pedrohma95@gmail.com>
2021-08-26 11:22:34 -0700

    fixing v3/items nextCursor

--- src/resources/Price.js
+++ src/resources/Price.js
@@ -1,26 +1,25 @@
-module.exports = function(httpClient, version){
-    let service = {
-        updateRegularPriceItem: updateRegularPriceItem,
-        updateBulkPrices: updateBulkPrices,
-        setUpCapSKU: setUpCapSKU,
-        setUpCapSKUAll: setUpCapSKUAll
-    };
-    return service;
+module.exports = function (httpClient, version) {
+  let service = {
+    updateRegularPriceItem: updateRegularPriceItem,
+    updateBulkPrices: updateBulkPrices,
+    setUpCapSKU: setUpCapSKU,
+    setUpCapSKUAll: setUpCapSKUAll,
+  };
+  return service;
 
+  async function updateRegularPriceItem() {
+    throw new Error("Not implemented exception.");
+  }
 
-    async function updateRegularPriceItem(){
-        throw new Error('Not implemented exception.')
-    }
+  async function updateBulkPrices() {
+    throw new Error("Not implemented exception.");
+  }
 
-    async function updateBulkPrices(){
-        throw new Error('Not implemented exception.')
-    }
+  async function setUpCapSKU() {
+    throw new Error("Not implemented exception.");
+  }
 
-    async function setUpCapSKU(){
-        throw new Error('Not implemented exception.')
-    }
-
-    async function setUpCapSKUAll(){
-        throw new Error('Not implemented exception.')
-    }
-}
+  async function setUpCapSKUAll() {
+    throw new Error("Not implemented exception.");
+  }
+};
--- src/resources/Promotion.js
+++ src/resources/Promotion.js
@@ -1,19 +1,19 @@
-module.exports = function(httpClient, version){
-    let service = {
-        getPromotionalPrices: getPromotionalPrices
-    };
-    return service;
+module.exports = function (httpClient, version) {
+  let service = {
+    getPromotionalPrices: getPromotionalPrices,
+  };
+  return service;
 
-    /**
-     * Retrieves a list of promotional prices for a single SKU.
-     * @param {String} sku 
-     */
-    async function getPromotionalPrices(sku){
-        try {
-            const response = await httpClient.get(`${version}/promo/sku/${sku}`);
-            return response.data;    
-        } catch (error) {
-            throw error;
-        }
+  /**
+   * Retrieves a list of promotional prices for a single SKU.
+   * @param {String} sku
+   */
+  async function getPromotionalPrices(sku) {
+    try {
+      const response = await httpClient.get(`${version}/promo/sku/${sku}`);
+      return response.data;
+    } catch (error) {
+      throw error;
     }
-}
+  }
+};
